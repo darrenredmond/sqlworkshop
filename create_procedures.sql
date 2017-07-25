9b7ed83c6efff0699dd77035ea0d60dadc7e730f
Darren Redmond <darren.redmondgmail.com>
2017-07-25 19:22:47 +0100

    add the create procedures and the changed insert data.

--- create_procedures.sql
+++ create_procedures.sql
@@ -12,8 +12,25 @@ IS
 BEGIN
   INSERT INTO title(title_id, title, description, rating, category, release_date)
   VALUES (title_id_seq.NEXTVAL, p_title, p_description, p_rating, p_category, p_release_date);
-  DBMS_OUTPUT.PUT_LINE('Inserted ' || SQL%ROWCOUNT || ' row.');
+  DBMS_OUTPUT.PUT_LINE('Inserted ' || SQL%ROWCOUNT || ' title row.');
 END;
 /
 
+-- Procedure to add a member - it takes a first name, last name, address, city,
+-- phone, and join date as its parameters and will insert a new record
+-- into the member table
+CREATE OR REPLACE PROCEDURE add_member(
+  p_first_name member.first_name%TYPE,
+  p_last_name member.last_name%TYPE,
+  p_address member.address%TYPE,
+  p_city member.city%TYPE,
+  p_phone member.phone%TYPE,
+  p_join_date member.join_date%TYPE)
+IS
+BEGIN
+  INSERT INTO member(member_id, first_name, last_name, address, city, phone, join_date)
+  VALUES (member_id_seq.NEXTVAL, p_first_name, p_last_name, p_address, p_city, p_phone, p_join_date);
+  DBMS_OUTPUT.PUT_LINE('Inserted ' || SQL%ROWCOUNT || ' member row.');
+END;
+/
 
--- insert_data.sql
+++ insert_data.sql
@@ -14,44 +14,16 @@ BEGIN
     'PG', 'DRAMA', TO_DATE( '12-SEP-1995','DD-MON-YYYY'));
   add_title('Soda Gang', 'After discovering a cache of drugs, a young couple find themselves pitted against a vicious gang.',
     'NR', 'ACTION', TO_DATE( '01-JUN-1995','DD-MON-YYYY'));
+  add_member('Carmen', 'Velasquez', '283 King Street', 'Seattle', '206-899-6666', TO_DATE('08-MAR-1990', 'DD-MON-YYYY'));
+  add_member('LaDoris', 'Ngao', '5 Modrany', 'Bratislava', '586-355-8882', TO_DATE('08-MAR-1990', 'DD-MON-YYYY'));
+  add_member('Midori', 'Nagayama', '68 Via Centrale', 'Sao Paolo', '254-852-5764', TO_DATE('17-JUN-1991', 'DD-MON-YYYY'));
+  add_member('Mark', 'Quick-toSee', '6921 King Way', 'Lagos', '63-559-7777', TO_DATE('07-APR-1990', 'DD-MON-YYYY'));
+  add_member('Audry', 'Ropeburn', '86 Chu Street', 'Hong Kong', '41-559-87', TO_DATE('18-JAN-1991', 'DD-MON-YYYY'));
+  add_member('Molly', 'Urguhart', '3035 Laurier', 'Quebec', '418-542-9988', TO_DATE('18-JAN-1991', 'DD-MON-YYYY'));
+
 END;
 /
 
-INSERT INTO member(member_id, first_name, last_name, address,
-city, phone, join_date)
-VALUES (member_id_seq.NEXTVAL, 'Carmen', 'Velasquez',
-'283 King Street', 'Seattle', '206-899-6666', TO_DATE('08-MAR-1990',
-'DD-MON-YYYY'));
-
-INSERT INTO member(member_id, first_name, last_name, address,
-city, phone, join_date)
-VALUES (member_id_seq.NEXTVAL, 'LaDoris', 'Ngao',
-'5 Modrany', 'Bratislava', '586-355-8882', TO_DATE('08-MAR-1990',
-'DD-MON-YYYY'));
-
-INSERT INTO member(member_id, first_name, last_name, address,
-city, phone, join_date)
-VALUES (member_id_seq.NEXTVAL, 'Midori', 'Nagayama',
-'68 Via Centrale', 'Sao Paolo', '254-852-5764', TO_DATE('17-JUN-1991',
-'DD-MON-YYYY'));
-  
-INSERT INTO member(member_id, first_name, last_name, address,
-city, phone, join_date)
-VALUES (member_id_seq.NEXTVAL, 'Mark', 'Quick-toSee',
-'6921 King Way', 'Lagos', '63-559-7777', TO_DATE('07-APR-1990',
-'DD-MON-YYYY'));
-
-INSERT INTO member(member_id, first_name, last_name, address,
-city, phone, join_date)
-VALUES (member_id_seq.NEXTVAL, 'Audry', 'Ropeburn',
-'86 Chu Street', 'Hong Kong', '41-559-87', TO_DATE('18-JAN-1991',
-'DD-MON-YYYY'));
-
-INSERT INTO member(member_id, first_name, last_name, address,
-city, phone, join_date)
-VALUES (member_id_seq.NEXTVAL, 'Molly', 'Urguhart',
-'3035 Laurier', 'Quebec', '418-542-9988', TO_DATE('18-JAN-1991',
-'DD-MON-YYYY'));
 
 --(select title_id from title where title = 'Willie and Christmast Too');
 
