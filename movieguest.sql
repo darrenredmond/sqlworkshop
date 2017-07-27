794a4150fa409c496543e634616fb72b62b4e650
Darren Redmond <darren.redmondgmail.com>
2017-07-27 20:00:07 +0100

    add the movie guest file.

--- movieguest.sql
+++ movieguest.sql
@@ -1,3 +1,24 @@
+-- AS SYS  
+grant create user to hr;
+grant create role to hr;
+grant all privileges to hr;
+
+-- AS HR  
+create user movieadmin
+identified by movieadmin;
+
+create user movieguest
+identified by movieguest;
+
+create role moviemanager;
+create role movienormal;
+
+grant create table, create session, create view
+    to moviemanager;
+
+grant select on employees to movieguest;
+
+grant connect, create session to movieguest;
 
 select * from hr.employees;
 
@@ -6,3 +27,4 @@ select * from user_role_privs;
 select * from user_sys_privs;
 
 select * from user_tab_privs_recd;
+
